676009d1764eb1e4862275f31fc40263b6ed2a57
Alan Chuan <alan@Alans.local>
2021-01-21 17:40:36 -0600

    Edits

---
+++ client/src/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant="success">Success</Button> <h1>home</h1>
+    </div>
+  );
+}
