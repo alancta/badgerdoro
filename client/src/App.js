da45c42b85f54bc412fa8bd8649a4671faac55fa
Riza Hassan <ruhulruzbihan@gmail.com>
2021-01-21 23:41:18 -0600

    Added route to components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,52 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React, {Fragment,useState,useEffect} from 'react';
+import {BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom";
 import Home from "./Home";
+import Dashboard from "./components/Dashboard";
+import Register from "./components/Register";
+import Login from "./components/Login";
 function App() {
+  const [isAuthenticated,setIsAuthenticated] = useState(false);
+
+  const setAuth =(boolean)=>{
+    setIsAuthenticated(boolean);
+  };
+
+  async function isAuth(){
+    try {
+      const response = await fetch("http://localhost:5000/auth/is-verify",{
+            method: "GET",
+            headers:{token:localStorage.token}});
+
+      const parseRes = await response.json();
+      console.log(parseRes);
+      parseRes === true ? setIsAuthenticated(true) : setIsAuthenticated(false);
+      console.log(isAuthenticated);
+
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+
+
+  useEffect((boolean)=>{
+    isAuth(boolean)
+  });
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div>
+        <Switch><Route exact path="/" render={Home} />
+        <Route exact path="/login" render={props => !isAuthenticated ? (<Login {...props} setAuth={setAuth}/>) :(<Redirect to="/dashboard" />)  } />
+            <Route exact path="/register" render={props => !isAuthenticated ? (<Register {...props} setAuth={setAuth}/>) : (<Redirect to="/login" />) } />
+            <Route exact path="/dashboard" render={props => isAuthenticated ? (<Dashboard {...props} setAuth={setAuth}/>) : (<Redirect to="/" />)  } /></Switch>
+        
+      </div>
+  </Router>
+    // <div className="App">
+    //   <Test />
+      
+    // </div>
   );
 }
 
