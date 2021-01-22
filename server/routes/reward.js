2d4a46c85d76c29564f108f0d789767ea0ccdfd7
Riza Hassan <ruhulruzbihan@gmail.com>
2021-01-21 23:45:20 -0600

    Reward logic

---
+++ server/routes/reward.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+const pool = require("../db");
+const authorization = require("../middleware/authorization");
+
+router.post("/", async (req, res) => {
+try {
+    console.log("reward system");
+
+    const {email, badgerbucks} = req.body;
+
+    if(badgerbucks<0){
+        return res.status(401).send("Invalid badgerbucks");
+    }
+
+    //Add the reward to the database 
+    const addReward = await pool.query(
+        "UPDATE rewards SET badgerbucks = badgerbucks + $2 WHERE user_email = $1",[email,badgerbucks]
+    );
+
+    //5. Test output
+    
+    console.log("Added reward");
+    res.json("Added reward");
+} catch (error) {
+    console.log(error.message);
+    res.status(500).send("Server Error");
+}
+
+
+
+});
+
+module.exports = router;
