28cd1518a793e8beb07f9ea3b944b37b3dcb74f5
Riza Hassan <ruhulruzbihan@gmail.com>
2021-01-22 03:34:44 -0600

    Added icon and logout

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,16 @@
 import React, { Fragment ,useEffect,useState} from "react";
+import {Container,Row,Col} from 'react-bootstrap';
+import UserIcon from './images/user'
+import Popover from 'react-bootstrap/Popover'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import Button from 'react-bootstrap/Button'
 
 const Dashboard = ({setAuth})=>{
     const[name,setName] = useState("");
-    // const[email,setEmail] = useState("");
+    const[email,setEmail] = useState("");
+    const[badgerbucks,setBadgerbucks] = useState("");
 
-    async function getName(){
+    async function getNameAndReward(){
         try {
             const response = await fetch("http://localhost:5000/dashboard/",{
                 method:"GET",
@@ -12,17 +18,17 @@ const Dashboard = ({setAuth})=>{
             });
 
             const parseRes = await response.json();
-            // console.log(parseRes);
 
-            setName(parseRes.user_name)
-            // setEmail(parseRes.user_email)
+            setName(parseRes.user_name);
+            setEmail(parseRes.user_email);
+            setBadgerbucks(parseRes.badgerbucks);
         } catch (error) {
             console.error(error.message)
         }
     }
 
     useEffect(() => {
-        getName();
+        getNameAndReward();
     },[]);
 
     const logout = (e) => {
@@ -30,12 +36,30 @@ const Dashboard = ({setAuth})=>{
         localStorage.removeItem("token");
         setAuth(false);
     }
+
+    const logoutPopover = (
+        <Popover id="popover-basic" >
+          <Popover.Content>
+              {name} <br></br> {email}<br></br> 
+          <button className="btn btn-danger mt-4" onClick={e=> logout(e)}>Logout</button>
+    </Popover.Content>
+        </Popover>
+      );
+
     return(
         <Fragment>
-        <h1>Dashboard</h1>
-        <h2>{name}</h2>
-        {/* <h3>{email}</h3> */}
-        <button className="btn btn-primary" onClick={e=> logout(e)}>Logout</button>
+            <Container className="mt-4 container-fluid">
+                <Row>
+                    <Col xs={9} ><h2>Dashboard</h2></Col>
+                    <Col xs={3}><OverlayTrigger  trigger="click" key="left" placement="left" overlay={logoutPopover}>
+                    <Button variant="outline-dark" ><UserIcon class="w-100"/></Button>
+  </OverlayTrigger></Col>
+                </Row>
+
+                </Container>
+
+            <div></div>
+        
 
         </Fragment>
     );
