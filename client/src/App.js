18628d2a8e08234ce509081c313c1c2bca31dc6c
Alan Chuan <alan@Alans.local>
2021-01-23 17:47:44 -0600

    Updated home page layout and buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,85 @@
 import "./App.css";
-import React, {useState,useEffect} from 'react';
-import {BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom";
-import Home from "./Home";
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+import Home from "./components/Home";
 import Dashboard from "./components/Dashboard";
 import Register from "./components/Register";
 import Login from "./components/Login";
 function App() {
-  const [isAuthenticated,setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  const setAuth =(boolean)=>{
+  const setAuth = boolean => {
     setIsAuthenticated(boolean);
   };
 
-  async function isAuth(){
+  async function isAuth() {
     try {
-      const response = await fetch("http://localhost:5000/auth/is-verify",{
-            method: "GET",
-            headers:{token:localStorage.token}});
+      const response = await fetch("http://localhost:5000/auth/is-verify", {
+        method: "GET",
+        headers: { token: localStorage.token }
+      });
 
       const parseRes = await response.json();
       console.log(parseRes);
       parseRes === true ? setIsAuthenticated(true) : setIsAuthenticated(false);
       console.log(isAuthenticated);
-
     } catch (error) {
       console.error(error.message);
     }
   }
 
-
-
-  useEffect((boolean)=>{
-    isAuth(boolean)
+  useEffect(boolean => {
+    isAuth(boolean);
   });
   return (
     <Router>
       <div>
-        <Switch><Route exact path="/" render={Home } />
-        <Route exact path="/login" render={props => !isAuthenticated ? (<Login {...props} setAuth={setAuth}/>) :(<Redirect to="/dashboard" />)  } />
-            <Route exact path="/register" render={props => !isAuthenticated ? (<Register {...props} setAuth={setAuth}/>) : (<Redirect to="/login" />) } />
-            <Route exact path="/dashboard" render={props => isAuthenticated ? (<Dashboard {...props} setAuth={setAuth}/>) : (<Redirect to="/" />)  } /></Switch>
-        
+        <Switch>
+          <Route exact path="/" render={Home} />
+          <Route
+            exact
+            path="/login"
+            render={props =>
+              !isAuthenticated ? (
+                <Login {...props} setAuth={setAuth} />
+              ) : (
+                <Redirect to="/dashboard" />
+              )
+            }
+          />
+          <Route
+            exact
+            path="/register"
+            render={props =>
+              !isAuthenticated ? (
+                <Register {...props} setAuth={setAuth} />
+              ) : (
+                <Redirect to="/login" />
+              )
+            }
+          />
+          <Route
+            exact
+            path="/dashboard"
+            render={props =>
+              isAuthenticated ? (
+                <Dashboard {...props} setAuth={setAuth} />
+              ) : (
+                <Redirect to="/" />
+              )
+            }
+          />
+        </Switch>
       </div>
-  </Router>
+    </Router>
     // <div className="App">
     //   <Test />
-      
+
     // </div>
   );
 }
